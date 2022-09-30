831f615249104ea82c85ca12425406528cffb5e8
chintamkavitha <kavitha.kavali@gmail.com>
2022-09-30 14:55:26 +0530

    employ data

---
+++ employdata.sh
@@ -0,0 +1,3 @@
+here we enter the employ details
+whats the name
+whats the role in team
---
+++ employsalary.java
@@ -0,0 +1,4 @@
+employs salary details can be saved here
+how days employs worked per month
+salary per day
+
