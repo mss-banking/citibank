36bd4be6e87edac28a957139e0cb332ccbfdbc23
chintamkavitha <kavitha.kavali@gmail.com>
2023-02-28 19:54:54 +0530

    IM UPDATING CODE BY USING CHERRY-PICK COMMAND

--- test.java
+++ test.java
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 im updating this file
+=======
+IM PRACTISING ON MERGE
+AND ALSO PULL
+im updating the file,for my practice
+>>>>>>> e53507e (IM USING FOR CHECKING MERGE AND PULL COMMAND)
 hello everyone
 hi to all
