c0d70ae2bf7326ff3a520e1f9239300c0741c37c
chintamkavitha <kavitha.kavali@gmail.com>
2023-03-10 16:16:26 +0530

    practising git commands

--- deploy.java
+++ deploy.java
@@ -1,4 +1,4 @@
-icant work with new1 repository,so im using  old
+PRACTICISNG
 hi im adding somemore code from dev branch
 im adding changed file to the branch
 we can change from one branch to another
