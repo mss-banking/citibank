bf580585cb9564e1e5e88373bac7eecb4bc0afc7
chintamkavitha <kavitha.kavali@gmail.com>
2023-04-24 17:27:03 +0530

    to check Revert "changed the code by testjava"
    This reverts commit ce2aa3651cd673f9efa2ec4fd3ea49a8f1eab66e.

--- test.java
+++ test.java
@@ -1,4 +1,14 @@
-THIS IS FROM TEST .JAVA FILE
+echo"im working on rebase"
+HELLLoo
+EVERYONE
+im updated for practising rebase
+<<<<<<< HEAD
+im updating this file
+=======
+IM PRACTISING ON MERGE
+AND ALSO PULL
 im updating the file,for my practice
+>>>>>>> e53507e (IM USING FOR CHECKING MERGE AND PULL COMMAND)
+>>>>>>> stage
 hello everyone
-
+hi to all
